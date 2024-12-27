2c5ef044997208644c787ed72d6cef08b857d6fd
zeniaparvin <zeniaparvin18@gmail.com>
2024-12-27 20:58:01 +0530

    first commit

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function handleLogin() {
     // Add login functionality here
     window.location.href = '/login';
 }
-
 // Add active class to current nav item
 document.addEventListener('DOMContentLoaded', () => {
     const currentPath = window.location.pathname;
